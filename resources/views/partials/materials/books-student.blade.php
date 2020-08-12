5a791ef54be587c7bb6f1df0a2c6522574c93200
YoselynCMME <yoselyncmme@gmail.com>
2020-08-11 21:38:59 -0500

    Interactivos tronco comun

--- resources/views/partials/materials/books-student.blade.php
+++ resources/views/partials/materials/books-student.blade.php
@@ -13,10 +13,32 @@
             </a>
         </div>
     </div>
+    <div class="mt-2 text-center">
+        <a id="btnMaterialSt" class="btn" target="_blank" href="https://majesticeducationdigital.com/moodle/login/index.php">
+            <i class="fa fa-download"></i><br>
+            {{ $book['category'] === 'comun' ? 'Material descargable':'Downloadable material' }}
+        </a>
+    </div>
+@else
+    @if($book['link_lessons'] !== '#')
+        <div class="row">
+            <div class="col-md-6">
+                <a id="btnMaterialSt" class="btn" href="{{ $book['link_lessons'] }}">
+                    <i class="fa fa-cubes"></i><br>
+                    Lessons
+                </a>
+            </div>
+            <div class="col-md-6">
+                <a id="btnMaterialSt" class="btn" target="_blank" href="https://majesticeducationdigital.com/moodle/login/index.php">
+                    <i class="fa fa-download"></i><br>
+                    {{ $book['category'] === 'comun' ? 'Material descargable':'Downloadable material' }}
+                </a>
+            </div>
+        </div>
+    @else
+        <a id="btnMaterialSt" class="btn" target="_blank" href="https://majesticeducationdigital.com/moodle/login/index.php">
+            <i class="fa fa-download"></i><br>
+            {{ $book['category'] === 'comun' ? 'Material descargable':'Downloadable material' }}
+        </a>
+    @endif
 @endif
-<div class="mt-2 text-center">
-    <a id="btnMaterialSt" class="btn" target="_blank" href="https://majesticeducationdigital.com/moodle/login/index.php">
-        <i class="fa fa-download"></i><br>
-        {{ $book['category'] === 'comun' ? 'Material descargable':'Downloadable material' }}
-    </a>
-</div>
--- resources/views/partials/materials/digital-learning.blade.php
+++ resources/views/partials/materials/digital-learning.blade.php
@@ -1,9 +1,12 @@
 @if($book['category'] === 'english')
-    <a id="btnSimple" class="btn mt-2" href="{{ $book['link_lessons'] }}">
-        <i class="fa fa-cubes"></i> Lessons
-    </a>
     <a id="btnSimple" class="btn mt-2" href="{{ $book['link_games'] }}">
         <i class="fa fa-gamepad"></i> Games
+    </a>
+@endif
+@if($book['link_lessons'] !== '#')
+    <a id="btnSimple" class="btn mt-2" href="{{ $book['link_lessons'] }}">
+        <i class="fa fa-cubes"></i> 
+        {{ $book['category'] === 'comun' ? 'Lecciones':'Lessons' }}
     </a>
 @endif
 <a id="btnSimple" class="btn mt-2" target="_blank" href="http://generate-exam-demo.herokuapp.com/login">
