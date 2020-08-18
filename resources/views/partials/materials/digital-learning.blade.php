c0a5bbd00942a6d940e44c5e2f651de8648f1760
YoselynCMME <yoselyncmme@gmail.com>
2020-08-18 15:15:57 -0500

    Correcion Horizontes Banco

--- resources/views/partials/materials/digital-learning.blade.php
+++ resources/views/partials/materials/digital-learning.blade.php
@@ -9,10 +9,12 @@
         {{ $book['category'] === 'comun' ? 'Lecciones':'Lessons' }}
     </a>
 @endif
-<a id="btnSimple" class="btn mt-2" target="_blank" href="http://generate-exam-demo.herokuapp.com/login">
-    <i class="fa fa-bank"></i>
-    {{ $book['category'] === 'comun' ? 'Banco de reactivos':'Reagent Bank' }}
-</a>
+@if($book['link_games'] !== 'NA')
+    <a id="btnSimple" class="btn mt-2" target="_blank" href="http://generate-exam-demo.herokuapp.com/login">
+        <i class="fa fa-bank"></i>
+        {{ $book['category'] === 'comun' ? 'Banco de reactivos':'Reagent Bank' }}
+    </a>
+@endif
 <a id="btnSimple" class="btn mt-2" target="_blank" href="https://majesticeducationdigital.com/moodle/login/index.php">
     <i class="fa fa-download"></i>
     {{ $book['category'] === 'comun' ? 'Material descargable':'Downloadable material' }}
